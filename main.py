5f0e986cddee1812a4ccefab32a94eb8a6effc5b
cehakanyilmaz <ce.hakanyilmaz@gmail.com>
2016-06-02 22:48:54 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import xmlDosya, systemFunc,guiFunc, datetime
-
-class anaFonk:
-    #if datetime.datetime.now() != systemFunc.systemFunc.b:
-    systemFunc.systemFunc.systemData()
-    guiFunc.guiFunc.showFunc()
